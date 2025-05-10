b0272761bc33f8230884c68d6e87a60e751752b3
Sanket Mudholkar <sanketmudhulkar98@gmail.com>
2025-05-10 16:53:18 +0530

    first commit

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+    return (
+        <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
+
+        </div>
+    )
+}
+
+export default Homepage
