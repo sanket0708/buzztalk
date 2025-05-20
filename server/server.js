8059fd6091df16fac5374a008213a5aa1b59e1e0
Sanket Mudholkar <sanketmudhulkar98@gmail.com>
2025-05-21 03:29:57 +0530

    fixing production bugs

--- server/server.js
+++ server/server.js
@@ -49,6 +49,11 @@ app.use("/api/messages", messageRouter);
 //connect mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
+
+//exporting for vercel
+export default server;
